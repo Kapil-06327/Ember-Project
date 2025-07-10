5add9619b603828cdea9e7cbc988f5eb1b11a6ce
kapilvijayakumar <kapilv3666@gmail.com>
2025-07-10 19:26:26 +0530

    'logout'

--- app/components/layout-wrapper.js
+++ app/components/layout-wrapper.js
@@ -1,10 +1,16 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import {action} from '@ember/object';
 
 export default class LayoutWrapperComponent extends Component {
   @service router;
 
   get isLoginRoute() {
     return this.router.currentRouteName === 'login';
+  }
+
+  @action
+  isLogOut() {
+    this.router.transitionTo('login');
   }
 }
